d3b47276c785db2c708f0819a5ff06c10cf0d0f2
Unknown <texcharly9@gmail.com>
2018-06-10 17:29:31 -0500

    Mas pojos documentados

--- src/model/pojos/Planeacion.java
+++ src/model/pojos/Planeacion.java
@@ -5,7 +5,8 @@ import javafx.beans.property.SimpleStringProperty;
 
 /**
  *
- * @author Deklok
+ * @author Daniel Escamilla Cortés
+ * @version 1.0
  */
 public class Planeacion {
   private Integer idPlaneacion;
@@ -15,9 +16,23 @@ public class Planeacion {
   private SimpleStringProperty tecnicasDidacticas = new SimpleStringProperty();
   private Integer idPlanDeCurso;
 
+    /**
+    * Constructor por default de la clase Planeacion.
+    */  
   public Planeacion() {
   }
 
+    /**
+    * Constructor de la clase con todos sus atributos.
+    * @param idPlaneacion Corresponde al identificador del objeto.
+    * @param fechas Corresponde a las fechas que estand destinadas para la planeacion.
+    * @param temas Corresponde a los temas que abarca la evaluación.
+    * @param unidad Corresponde a la unidad de la cual es parte la planeacion.
+    * @param tecnicasDidacticas Corresponde a las tecnicas que se utilizan para
+    * planeacion.
+    * @param idPlanDeCurso Corresponde al identificador del Plan de Curso 
+    * asociado a la planeacion.
+    */  
   public Planeacion(Integer idPlaneacion, String fechas, String temas, Integer unidad, String tecnicasDidacticas,
       Integer idPlanDeCurso) {
     this.idPlaneacion = idPlaneacion;
@@ -28,6 +43,14 @@ public class Planeacion {
     this.idPlanDeCurso = idPlanDeCurso;
   }
 
+    /**
+    * Constructor de la clase con solo algunos de sus atributos.
+    * @param fechas Corresponde a las fechas que estand destinadas para la planeacion.
+    * @param temas Corresponde a los temas que abarca la evaluación.
+    * @param unidad Corresponde a la unidad de la cual es parte la planeacion.
+    * @param tecnicasDidacticas Corresponde a las tecnicas que se utilizan para
+    * planeacion.
+    */  
   public Planeacion(String fechas, String temas, Integer unidad, String tecnicasDidacticas) {
     this.fechas = new SimpleStringProperty(fechas);
     this.temas = new SimpleStringProperty(temas);
@@ -35,50 +58,100 @@ public class Planeacion {
     this.tecnicasDidacticas = new SimpleStringProperty(tecnicasDidacticas);
   }
 
+  /**
+   *Retorna el identificador del objeto
+   * @return idPlaneacion
+   */
   public Integer getIdPlaneacion() {
     return idPlaneacion;
   }
 
+  /**
+   *Establece el identificador de la planeacion.
+   * @param idPlaneacion Corresponde al identificador de la planeacion.
+   */
   public void setIdPlaneacion(Integer idPlaneacion) {
     this.idPlaneacion = idPlaneacion;
   }
 
+    /**
+   *Retorna las fechas que hay dentro de la planeacion
+   * @return fechas
+   */
   public String getFechas() {
     return fechas.get();
   }
 
+  /**
+   *Establece las fechas en las que se lleva a cabo la planeacion.
+   * @param fechas Corresponde a las fechas que abarca la planeacion.
+   */
   public void setFechas(String fechas) {
     this.fechas = new SimpleStringProperty(fechas);
   }
 
+    /**
+   *Retorna los temas de la Planeacion
+   * @return temas
+   */
   public String getTemas() {
     return temas.get();
   }
 
+  /**
+   *Establece los temas que cubre la planeacion
+   * @param temas Corresponde a los temas que abarca la planeacion.
+   */
   public void setTemas(String temas) {
     this.temas = new SimpleStringProperty(temas);
   }
 
+    /**
+   *Retorna la unidad a la que pertenece la Planeacion
+   * @return unidad
+   */
   public Integer getUnidad() {
     return unidad.get();
   }
 
+  /**
+   *Establece la unidad de la que es parte la planeacion.
+   * @param unidad Corresponde a la unidad a la que pertenece la planeacion.
+   */
   public void setUnidad(Integer unidad) {
     this.unidad = new SimpleIntegerProperty(unidad);
   }
 
+    /**
+   *Retorna las tecnicas didacticas que pertenecen a la planeacion.
+   * @return tecnicasDidacticas
+   */
   public String getTecnicasDidacticas() {
     return tecnicasDidacticas.get();
   }
 
+  /**
+   *Establece las tecnicas didacticas que contiene
+   * @param tecnicasDidacticas Corresponde a las tecnicas didacticas que estan
+   * dentro de la planeacion.
+   */
   public void setTecnicasDidacticas(String tecnicasDidacticas) {
     this.tecnicasDidacticas = new SimpleStringProperty(tecnicasDidacticas);
   }
 
+    /**
+   *Retorna el identificador del plan de curso al que pertenece la planeacion.
+   * @return idPlanDeCurso
+   */
   public Integer getIdPlanDeCurso() {
     return idPlanDeCurso;
   }
 
+    /**
+   *Establece el identificador del plan de curso al que pertence la planeacion.
+   * @param idPlanDeCurso Corresponde al identificador del plan de curso al que
+   * pertenece la planeacion.
+   */
   public void setIdPlanDeCurso(Integer idPlanDeCurso) {
     this.idPlanDeCurso = idPlanDeCurso;
   }
--- src/model/pojos/Reunion.java
+++ src/model/pojos/Reunion.java
@@ -4,7 +4,8 @@ import java.util.Date;
 
 /**
  *
- * @author Deklok
+ * @author Daniel Escamilla Cortés 
+ * @version 1.0
  */
 public class Reunion {
     private Integer idReunion;
@@ -14,10 +15,24 @@ public class Reunion {
     private String informacionAdicional;
     private String lugar;
     private Integer idMinutaAcademia;
-
+     /**
+    * Constructor por default de la clase.
+    */
     public Reunion() {
     }
-
+    /**
+    * Constructor de la clase con todos sus atributos.
+    * @param idReunion Corresponde la identificador en numero entero del objeto.
+    * @param nombre Corresponde al nombre que se le quiera asignar a la reunión.
+    * @param fechaInicio Corresponde a la fecha en la que da inicio la reunion.
+    * @param fechaFin Corresponde a la fecha en la que finaliza la reunión.
+    * @param informacionAdicional Corresponde a los comentarios u observaciones
+    * que se quieran hacer respecto a la reunión.
+    * @param lugar Corresponde a la zona, o lugar especifico donde se llevó a 
+    * cabo la reunión.
+    * @param idMinutaAcademia Corresponde al identificador de la minuta de 
+    * academia que se creará apartir de esta reunion.
+    */    
     public Reunion(Integer idReunion, String nombre, Date fechaInicio, Date fechaFin, String informacionAdicional, String lugar, Integer idMinutaAcademia) {
         this.idReunion = idReunion;
         this.nombre = nombre;
@@ -28,58 +43,118 @@ public class Reunion {
         this.idMinutaAcademia = idMinutaAcademia;
     }
 
+     /**
+    * Retorna el identificador del objeto.
+    * @return idReunion
+    */
     public Integer getIdReunion() {
         return idReunion;
     }
 
+    /**
+    * Establece el valor para el identificador de la reunion.
+    * @param idReunion Corresponde al identificador que tendrá la reunion.
+    */
     public void setIdReunion(Integer idReunion) {
         this.idReunion = idReunion;
     }
-
+    
+    /**
+    * Retorna el nombre que s ele haya otorgado a la reunion.
+    * @return nombre
+    */
     public String getNombre() {
         return nombre;
     }
 
+    /**
+    * Establece el valor para el nombre de la reunion.
+    * @param nombre Corresponde al nombre que tendrá la reunion.
+    */    
     public void setNombre(String nombre) {
         this.nombre = nombre;
     }
 
+    /**
+    * Retorna la fecha en que inició la reunión.
+    * @return fechaInicio
+    */
     public Date getFechaInicio() {
         return fechaInicio;
     }
 
+    /**
+    * Establece el valor para la fecha de inicio de la reunion.
+    * @param fechaInicio Corresponde a la fecha de inicio que tendrá la reunion.
+    */
     public void setFechaInicio(Date fechaInicio) {
         this.fechaInicio = fechaInicio;
     }
 
+    /**
+    * Retorna la fecha en que finalizó la reunión.
+    * @return fechaFin
+    */
     public Date getFechaFin() {
         return fechaFin;
     }
 
+    /**
+    * Establece el valor para fecha de término de la reunion.
+    * @param fechaFin Corresponde a la fecha de término que tendrá la reunion.
+    */
     public void setFechaFin(Date fechaFin) {
         this.fechaFin = fechaFin;
     }
 
+    /**
+    * Retorna los comentarios u observaciones que se hayan hecho
+    * durante la reunion.
+    * @return informacionAdicional
+    */
     public String getInformacionAdicional() {
         return informacionAdicional;
     }
 
+    /**
+    * Establece el valor para la informacion adicional de la reunion.
+    * @param infromacionAdicional Corresponde a las observaciones o comentarios que tendrá la reunion.
+    */    
     public void setInformacionAdicional(String informacionAdicional) {
         this.informacionAdicional = informacionAdicional;
     }
 
+    /**
+    * Retorna el lugar donde se llevó a cabo la reunion.
+    * @return lugar
+    */
     public String getLugar() {
         return lugar;
     }
 
+    /**
+    * Establece el valor para el lugar de la reunion.
+    * @param lugar Corresponde al lugar donde se llevó a cabo la reunion.
+    */    
     public void setLugar(String lugar) {
         this.lugar = lugar;
     }
 
+    /**
+    * Retorna el identificador de la minuta de academia 
+    * asociada a la reunion.
+    * @return idMinutaAcademia
+    */
     public Integer getIdMinutaAcademia() {
         return idMinutaAcademia;
     }
 
+    /**
+    * Establece el valor para el identificador de la minuta de academia que se
+    * relaciona con lareunion.
+    * @param idMinutaAcademia Corresponde al identificador de la minuta de 
+    * acadmeia que tendrá la reunion.
+    */    
     public void setIdMinutaAcademia(Integer idMinutaAcademia) {
         this.idMinutaAcademia = idMinutaAcademia;
     }
