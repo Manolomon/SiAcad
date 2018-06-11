9f8af5316bd1ac5c7126d3ec89b2acf27429a5f4
Manolo Perez <manolope338@gmail.com>
2018-06-10 23:27:44 -0500

    Hala vida alguien encuentre el error plz

--- src/controller/PlanDeTrabajoController.java
+++ src/controller/PlanDeTrabajoController.java
@@ -24,11 +24,11 @@ import javafx.collections.FXCollections;
 import javafx.collections.ListChangeListener;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Scene;
+import javafx.scene.control.Tab;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
@@ -49,173 +49,158 @@ import model.pojos.Maestro;
 public class PlanDeTrabajoController implements Initializable {
 
   @FXML
-    private StackPane rootPane;
+  private StackPane rootPane;
 
-    @FXML
-    private JFXListView<AnchorPane> listParticipantes;
+  @FXML
+  private JFXListView<AnchorPane> listParticipantes;
 
-    @FXML
-    private AnchorPane panelTopBar;
+  @FXML
+  private AnchorPane panelTopBar;
 
-    @FXML
-    private JFXButton btnHamburger;
+  @FXML
+  private JFXButton btnHamburger;
 
-    @FXML
-    private JFXButton btnEnviar;
+  @FXML
+  private JFXButton btnEnviar;
 
-    @FXML
-    private JFXButton btnGuardar;
+  @FXML
+  private JFXButton btnGuardar;
 
-    @FXML
-    private JFXButton btnMenosActividades;
+  @FXML
+  private JFXButton btnMenosActividades;
 
-    @FXML
-    private JFXButton btnMasActividades;
+  @FXML
+  private JFXButton btnMasActividades;
 
-    @FXML
-    private JFXTextArea txtObjetivoParticular;
+  @FXML
+  private JFXTextArea txtObjetivoParticular;
 
-    @FXML
-    private JFXTextArea txtMeta;
+  @FXML
+  private JFXTextArea txtMeta;
 
-    @FXML
-    private TableView<Actividad> tableActividad;
-            
-    @FXML
-    private TableColumn<Actividad, String> actividadActividad;
+  @FXML
+  private TableView<Actividad> tableActividad;
 
-    @FXML
-    private TableColumn<Actividad, String> actividadFechas;
+  @FXML
+  private TableColumn<Actividad, String> actividadActividad;
 
-    @FXML
-    private TableColumn<Actividad, String> actividadFormaOperar;
+  @FXML
+  private TableColumn<Actividad, String> actividadFechas;
 
-    @FXML
-    private JFXTabPane tabPanelEE;
+  @FXML
+  private TableColumn<Actividad, String> actividadFormaOperar;
 
-    @FXML
-    private JFXTextArea txtObjetivoGeneral;
-    
-    private List<Maestro> listaParticipantes;
-    
-    ////////////////////////////////////////////////////////////////////////////
+  @FXML
+  private JFXTabPane tabPanelEE;
 
-    @FXML
-    private JFXButton btnMenosEvaluacion;
+  @FXML
+  private JFXTextArea txtObjetivoGeneral;
 
-    @FXML
-    private JFXButton btnMasEvaluacion;
+  private List<Maestro> listaParticipantes;
 
-    @FXML
-    private JFXTextArea txtPrimerParcial;
+  ////////////////////////////////////////////////////////////////////////////
 
-    @FXML
-    private JFXTextArea txtSegundoParcial;
+  @FXML
+  private JFXButton btnMenosEvaluacion;
 
-    @FXML
-    private JFXTextArea txtPosterior;
+  @FXML
+  private JFXButton btnMasEvaluacion;
 
-    @FXML
-    private TableView<Evaluacion> tableEvaluacion;
+  @FXML
+  private JFXTextArea txtPrimerParcial;
 
-    @FXML
-    private TableColumn<Evaluacion, String> evaluacionCriterio;
+  @FXML
+  private JFXTextArea txtSegundoParcial;
 
-    @FXML
-    private TableColumn<Evaluacion, Integer> evaluacionPorcentaje;
-    
-    ////////////////////////////////////////////////////////////////////////////
-    
-    private ObservableList<Actividad> listaActividades;
-    
-    private int posicionTablaActividad;
+  @FXML
+  private JFXTextArea txtPosterior;
 
-    @FXML
-    void clickMasEvaluacion(ActionEvent event) {
+  @FXML
+  private TableView<Evaluacion> tableEvaluacion;
 
+  @FXML
+  private TableColumn<Evaluacion, String> evaluacionCriterio;
+
+  @FXML
+  private TableColumn<Evaluacion, Integer> evaluacionPorcentaje;
+
+  private ObservableList<Evaluacion> listaEvaluaciones;
+
+  private int posicionTablaEvalucaion;
+
+  private ObservableList<Actividad> listaActividades;
+
+  private int posicionTablaActividad;
+
+  private final ListChangeListener<Actividad> selectorTablaActividades = new ListChangeListener<Actividad>(){@Override public void onChanged(ListChangeListener.Change<?extends Actividad>c){ponerActividadSeleccionada();}};
+  
+  public Actividad getTablaActividadSeleccionada() {
+    if (tableActividad != null) {
+      List<Actividad> tabla = tableActividad.getSelectionModel().getSelectedItems();
+      if (tabla.size() == 1) {
+        final Actividad competicionSeleccionada = tabla.get(0);
+        return competicionSeleccionada;
+      }
     }
+    return null;
+  }
 
-    @FXML
-    void clickMenosEvaluacion(ActionEvent event) {
-        System.out.println("Holis");
+  private void ponerActividadSeleccionada() {
+    final Actividad act = getTablaActividadSeleccionada();
+    posicionTablaActividad = listaActividades.indexOf(act);
+    if (act != null) {
     }
-    
-    ////////////////////////////////////////////////////////////////////////////
-    
-    private final ListChangeListener<Actividad> selectorTablaActividades = new ListChangeListener<Actividad>() {
-        @Override
-        public void onChanged(ListChangeListener.Change<? extends Actividad> c) {
-            ponerActividadSeleccionada();
-        }
-    };
-    
-    public Actividad getTablaActividadSeleccionada() {
-        if (tableActividad != null) {
-          List<Actividad> tabla = tableActividad.getSelectionModel().getSelectedItems();
-          if (tabla.size() == 1) {
-            final Actividad competicionSeleccionada = tabla.get(0);
-            return competicionSeleccionada;
-          }
-        }
-        return null;
+  }
+
+  private void inicializarTablaActividad() {
+    actividadActividad.setCellValueFactory(new PropertyValueFactory<Actividad, String>("accion"));
+    actividadFechas.setCellValueFactory(new PropertyValueFactory<Actividad, String>("fecha"));
+    actividadFormaOperar.setCellValueFactory(new PropertyValueFactory<Actividad, String>("formaDeOperar"));
+    listaActividades = FXCollections.observableArrayList();
+    tableActividad.setItems(listaActividades);
+  }
+
+  @FXML
+  void clickEnviar(ActionEvent event) {
+
+  }
+
+  @FXML
+  void clickGuardar(ActionEvent event) {
+
+  }
+
+  @FXML
+  void clickMasActividad(ActionEvent event) {
+    JFXDialogLayout content = new JFXDialogLayout();
+    content.setHeading(new Text("Actividades"));
+    FXMLLoader loader = new FXMLLoader();
+    loader.setLocation(getClass().getResource("/view/Dialog/ActividadDialog.fxml"));
+    try {
+      loader.load();
+    } catch (IOException ex) {
+      Logger.getLogger(PlanDeTrabajoController.class.getName()).log(Level.SEVERE, null, ex);
     }
-    
-    private void ponerActividadSeleccionada() {
-        final Actividad act = getTablaActividadSeleccionada();
-            posicionTablaActividad = listaActividades.indexOf(act);
-        if (act != null) {
-        }
-    }
-    
-    private void inicializarTablaPlaneacion() {
-        actividadActividad.setCellValueFactory(new PropertyValueFactory<Actividad, String>("accion"));
-        actividadFechas.setCellValueFactory(new PropertyValueFactory<Actividad, String>("fecha"));
-        actividadFormaOperar.setCellValueFactory(new PropertyValueFactory<Actividad, String>("formaDeOperar"));
-        listaActividades = FXCollections.observableArrayList();
-        tableActividad.setItems(listaActividades);
-    }
+    ActividadDialogController display = loader.getController();
+    AnchorPane p = loader.getRoot();
+    content.setBody(p);
+    JFXDialog dialog = new JFXDialog(rootPane, content, JFXDialog.DialogTransition.CENTER);
+    JFXButton aceptar = new JFXButton("ACEPTAR");
 
-    @FXML
-    void clickEnviar(ActionEvent event) {
+    aceptar.setOnAction((ActionEvent e) -> {
+      Actividad act = display.crearActividad();
+      listaActividades.add(act);
+      dialog.close();
+    });
 
-    }
+    content.setActions(aceptar);
+    dialog.show();
+  }
 
-    @FXML
-    void clickGuardar(ActionEvent event) {
+  @FXML
+  void clickMenosActividad(ActionEvent event) {
 
-    }
-
-    @FXML
-    void clickMasActividad(ActionEvent event) {
-        JFXDialogLayout content = new JFXDialogLayout();
-        content.setHeading(new Text("Actividades"));
-        FXMLLoader loader = new FXMLLoader();
-        loader.setLocation(getClass().getResource("/view/Dialog/ActividadDialog.fxml"));
-        try {
-          loader.load();
-        } catch (IOException ex) {
-          Logger.getLogger(PlanDeTrabajoController.class.getName()).log(Level.SEVERE, null, ex);
-        }
-        ActividadDialogController display = loader.getController();
-        AnchorPane p = loader.getRoot();
-        content.setBody(p);
-        JFXDialog dialog = new JFXDialog(rootPane, content, JFXDialog.DialogTransition.CENTER);
-        JFXButton aceptar = new JFXButton("ACEPTAR");
-
-        aceptar.setOnAction((ActionEvent e) -> {
-            Actividad act = display.crearActividad();
-            listaActividades.add(act);
-            dialog.close();
-        });
-
-        content.setActions(aceptar);
-        dialog.show();
-    }
-
-    @FXML
-    void clickMenosActividad(ActionEvent event) {
-
-    }
+  }
 
   @FXML
   public void clickHamburger(ActionEvent event) {
@@ -233,6 +218,7 @@ public class PlanDeTrabajoController implements Initializable {
 
   @Override
   public void initialize(URL url, ResourceBundle rb) {
+    inicializarTablaActividad();
     for (int i = 0; i < 17; i++) {
       FXMLLoader loader = new FXMLLoader();
       loader.setLocation(getClass().getResource("/view/AsistenteAReunion.fxml"));
@@ -247,21 +233,22 @@ public class PlanDeTrabajoController implements Initializable {
       listParticipantes.getItems().add(pan);
     }
 
-    // String[] nombres = { "Requerimientos de Software", "Verificación y Validación de Software", "Principos de Diseño de Software"};
+    String[] nombres = { "Requerimientos de Software", "Verificación y Validación de Software", "Principos de Diseño de Software"};
 
-    // for (String nombre : nombres) {
-    //   FXMLLoader loader = new FXMLLoader();
-    //   loader.setLocation(getClass().getResource("/view/FormaDeEvaluacion.fxml"));
-    //   try {
-    //     loader.load();
-    //   } catch (IOException ex) {
-    //     Logger.getLogger(PlanDeTrabajoController.class.getName()).log(Level.SEVERE, null, ex);
-    //   }
-    //   //AsistenteAReunionController display = loader.getController();
-    //   AnchorPane p = loader.getRoot();
-    //   Tab tab = new Tab(nombre);
-    //   tab.setContent(p);
-    //   tabPanelEE.getTabs().add(tab);
-    // }
+    for (String nombre : nombres) {
+//    FXMLLoader loader = new FXMLLoader();
+//    loader.setLocation(getClass().getResource("/view/FormaDeEvaluacion.fxml"));
+//    try {
+//    loader.load();
+//    } catch (IOException ex) {
+//    Logger.getLogger(PlanDeTrabajoController.class.getName()).log(Level.SEVERE,
+//    null, ex);
+//    }
+    //AsistenteAReunionController display = loader.getController();
+    //AnchorPane p = loader.getRoot();
+    Tab tab = new Tab(nombre);
+    //tab.setContent(p);
+    tabPanelEE.getTabs().add(tab);
+    }
   }
 }
